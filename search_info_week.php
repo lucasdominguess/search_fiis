d091682964222432ad90d533aa8fe471d08c22b5
Lucas Joaquim Domingues x492420 <x492420@AR-PRODAM-00093.prodam.pref>
2025-12-18 16:25:21 -0300

    create new script

---
+++ search_info_week.php
@@ -0,0 +1,30 @@
+<?php
+require 'vendor/autoload.php';
+require 'config.php';
+
+use App\classes\SearchFII;
+use App\classes\CreateLogger;
+
+$bot = new CreateLogger();
+$searchFII = new SearchFII();
+$env = ENV;
+$channel = $env['CHANNEL02'];
+
+$tickets = [
+   ['VGIR11', 59, 'fii', 0.13],
+   # ['WHGR11', 425, 'fii', 0.10],
+   # ['VGHF11', 222, 'fii', 0.08],
+    ['PETR4', 4, 'acao', 0.63],
+    ['BPML11', 163, 'fii']
+];
+$msg = '';
+$dataArray = [];
+foreach ($tickets as $ticket) {
+    $msg = $searchFII->searchFII_rangeDate($ticket[0], $ticket[1], '7');
+
+    try {
+        $bot->loggerTelegram('', $msg, 'info', $channel);
+    } catch (\Throwable $th) {
+        $bot->loggerTelegram('', $th->getMessage(), 'error');
+    }
+}
